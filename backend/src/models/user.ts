6d7ced24617836d49bacab37630fb7289562b350
niteshmishra <nmishra4@ncsu.edu>
2024-01-20 10:23:05 -0500

    Add Login SignUp functionality - Backend

---
+++ backend/src/models/user.ts
@@ -0,0 +1,35 @@
+/**
+ * User document
+ */
+import mongoose from 'mongoose';
+import bcrypt from "bcryptjs";
+
+export type UserType = {
+    _id: string;
+    email: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+};
+
+const userSchema = new mongoose.Schema({
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true },
+    firstName: {type: String, required: true },
+    lastName: {type: String, required: true }
+})
+
+/** 
+ * This will tell mongo before doing "SAVE" function check if password is modified if yes then just encrypt it and save. 
+ * next() - tells do whatever task is next in the execution. NOT IMP
+ */
+userSchema.pre( "save", async function (next) {
+    if (this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 8)
+    }
+    next();
+} );
+
+const User = mongoose.model<UserType>("User", userSchema);
+
+export default User;
---
+++ backend/src/routes/users.ts
@@ -0,0 +1,55 @@
+import express, {Request, Response} from 'express';
+import User from '../models/user';
+import jwt from "jsonwebtoken";
+import {check, validationResult } from "express-validator";
+
+const router = express.Router();
+
+// /api/users/register
+router.post("/register", 
+    [
+    check("firstName", "First Name is required").isString(), //check if passed and string
+    check("lastName", "Last Name is required").isString(), //check if passed and string
+    check("email", "Email is required").isEmail(), //check if passed and string
+    check("password", "Password with 6 or more characters is required").isLength({min:6}), //check if passed and and min lenght 6
+    ], 
+    async (req: Request, res: Response) => {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()){
+        return res.status(400).json({message: errors.array()[0].msg});
+    }
+    
+    try{
+        let user = await User.findOne({
+            email:req.body.email,
+        });
+
+        if (user) {
+            return res.status(400).json({ message:"User already exists."});
+        }
+
+        user = new User(req.body)
+        await user.save();
+
+        const token = jwt.sign(
+            {userId: user.id}, 
+            process.env.JWT_SECRET_KEY as string,
+            {expiresIn: "1d",} //after one day it will expire
+            );
+
+        res.cookie("auth_token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production", //if production secure will be TRUE else False
+            maxAge: 86400000, //same as 1d but in millisecond
+        })
+
+        return res.status(200).send({ message: "User Registered Successfully!"});
+
+    }catch(error){
+        console.log(error);
+        res.status(500).send({ message: "Something went wrong."});
+    }
+})
+
+export default router;
